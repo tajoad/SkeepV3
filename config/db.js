eba18bc71581220c9f80f692bc50f5d132ce005f
tajoad <tajoad@gmail.com>
2023-08-12 15:42:38 +0100

    first commit

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
