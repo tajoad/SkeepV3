759aa66dde8d4af1650a47703544f22bf0300895
tajoad <tajoad@gmail.com>
2023-08-12 15:46:49 +0100

    first commit

--- config/db.js
+++ config/db.js
@@ -8,9 +8,12 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(
+      `mongodb+srv://skeep:skeep@cluster0.4etj4aj.mongodb.net/Skeep`,
+      {
+        useNewUrlParser: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (err) {
     console.log(err);
